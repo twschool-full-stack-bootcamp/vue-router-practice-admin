1fa5eb40b1e68c71ab2c8a5ff20a57711ca46786
Lin Ning <nlin@thoughtworks.com>
2019-09-10 17:58:34 +0800

    add mock data for a product list

--- src/pages/admin/Products.vue
+++ src/pages/admin/Products.vue
@@ -1,13 +1,28 @@
 <template>
     <div>
         Products
+1
+        <ul>
+            <li v-for="product in products" :key="product.id">
+                <!-- the link can be clicked and redirect to detail page with parameter -->
+                <a href="#">{{product.name}}</a>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
     export default {
         name: 'Products',
-        components: {
+        components: {},
+        data: function () {
+            return {
+                products: [
+                    {id: 'xxx1', name: 'Soda', price: 2},
+                    {id: 'xxx2', name: 'Cola', price: 2},
+                    {id: 'xxx3', name: 'Pure Water', price: 1}
+                ]
+            }
         }
     }
 </script>
